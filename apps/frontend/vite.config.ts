a71aa546ce182841db92aa17de3b23a9761f85e5
Martin Boucnik <xboucnik@fi.muni.cz>
2026-04-20 18:52:30 +0200

    Start the app and create hello world

--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { TanStackRouterVite } from '@tanstack/router-vite-plugin';
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-  plugins: [TanStackRouterVite(), react()],
+  plugins: [TanStackRouterVite(), react(), tailwindcss()],
   server: {
     port: 3000,
     proxy: {
